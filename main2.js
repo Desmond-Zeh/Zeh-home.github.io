a9bd75b8c3736dc87ab69b33f8eb1bcdccb60bf5
Desmond-Zeh <148614390+Desmond-Zeh@users.noreply.github.com>
2023-11-02 00:18:17 +0100

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,40 @@
+/* My main js code*/
+
+/* Wait for the page to be fully loaded before the image switching operation commences*/
+window.onload = () =>{
+ CommenceEvent();
+};
+/* Declare the main variables to be used in this program */
+let myImagesArray = ["TC.png","TT.png","SL7000.jpg","RS232.jpg","CB.png","surge.png"];
+let myImageIndex = 0;
+let timeInterval = 2000;
+/* CommenceEvent main function*/
+function CommenceEvent(){
+document.myImages.src=myImagesArray[myImageIndex];
+
+if(myImageIndex < myImagesArray.length - 1){   /// Verify if the myImageIndex is equal to the last array index
+myImageIndex ++;
+}
+else{
+ResetImages();
+}
+ setTimeout("CommenceEvent()", timeInterval);//// Set the time delay to 2s
+ }
+
+/* My main reset function*/
+function ResetImages(){
+myImageIndex = 0;
+  }
+/* Open a new window to display the following document links when clicked on the DOCUMENT tab in the Navigation bar*/
+
+let mydoc = document.getElementById("myDoc");
+mydoc.onclick = () =>{
+window.location.replace("https://desmond-zeh.github.io/Des.github.io/");
+};
+ let myhom = document.getElementById("home");
+myhom.onclick = () =>{
+window.location.replace("");
+};
+
+
+  
