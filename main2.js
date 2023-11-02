24a44addcd3386209e9e73557943498e717a93c5
Desmond-Zeh <148614390+Desmond-Zeh@users.noreply.github.com>
2023-11-02 01:57:43 +0100

    Update main2.js

--- main2.js
+++ main2.js
@@ -25,16 +25,25 @@ ResetImages();
 function ResetImages(){
 myImageIndex = 0;
   }
+
 /* Open a new window to display the following document links when clicked on the DOCUMENT tab in the Navigation bar*/
 
 let mydoc = document.getElementById("myDoc");
 mydoc.onclick = () =>{
-window.location.replace("https://desmond-zeh.github.io/Des.github.io/");
+window.location.replace("https://desmond-zeh.github.io/document.github.io/","_blank");
 };
  let myhom = document.getElementById("home");
 myhom.onclick = () =>{
-window.location.replace("");
+window.location.replace("https://desmond-zeh.github.io/Zeh-home.github.io/", "_blank");
 };
 
+let mydown = document.getElementById("down");
+mydown.onclick = () =>{
+window.location.replace("");
+};
+ let myabout = document.getElementById("about");
+myabout.onclick = () =>{
+window.location.replace("");
+};
 
   
